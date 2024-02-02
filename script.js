23d94427d4972833caa738ba36576157f5508927
Marie Wulff <mawu0001@stud.kea.dk>
2024-02-02 10:14:57 +0100

    template added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("LEts Go!");
+window.addEventListener("DOMContentLoaded", init);
+
+const beerURL = "https://api.punkapi.com/v2/beers";
+
+function init() {
+  console.log("init");
+  fetch(beerURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      console.log("json", json);
+    });
+}
